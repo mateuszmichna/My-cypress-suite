da38c549861b6b7e5099627574cd3de6b3bb797c
Mateusz Michna <mateusz.michna@venturedevs.com>
2021-08-19 16:36:21 +0200

    first dummy tests

---
+++ cypress/fixtures/email_sending_page_selectors.js
@@ -0,0 +1,21 @@
+export var emailSendingPageSelectors = {
+    
+    email_field: '#newsletter_email',
+    email_field_title: 'label[title="E-mail"]',
+    name_field: '#newsletter_name',
+    name_field_title: 'label[title="First name"]',
+    surname_field: '#newsletter_surname',
+    surname_field_title: 'label[title="Surname"]',
+    newsletter_type_field: '#newsletter_newsType',
+    newsletter_type_field_title: 'label[title="Newsletter type"]',
+    start_date_field: 'newsletter_startDate',
+    start_date_field_title: 'label[title="Start date"]',
+    end_date_field: '#newsletter_endDate',
+    end_date_field_title: 'label[title="End date"]',
+    sex_buttons: '#newsletter_Sex > .ant-radio-wrapper > .ant-radio',
+    sex_buttons_title: 'label[title="Sex"]',
+    agreement_checkmark: '#newsletter_agreement',
+    agreement_link: 'a',
+    submit_button: 'button[type=submit]',
+    footer: 'footer'
+}
---
+++ cypress/fixtures/email_sending_page_utils.js
@@ -0,0 +1,10 @@
+export var emailSendingPageUtils = {
+    proper_url: 'qa-recruitment-newsletter.s3-website-eu-west-1.amazonaws.com',
+    footer_copy: 'The Software House ©2020 - QAA recruitment task',
+    email_field_copy: 'E-mail',
+    first_name_copy: 'First name',
+    surname_copy: 'Surname',
+    newsletter_type_copy: 'Newsletter type',
+    sex_buttons_copy: 'Sex',
+    agreement_link: 'https://google.pl'
+}
