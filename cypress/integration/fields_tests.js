c87fa19595537eb81eaad2e9ecbe8c24155186db
Mateusz Michna <mateusz.michna@venturedevs.com>
2021-08-24 10:03:11 +0200

    newsletter field tests

--- cypress/integration/fields_tests.js
+++ cypress/integration/fields_tests.js
@@ -21,4 +21,30 @@ describe('Fields errors tests', () => {
       cy.get(emailSendingPageSelectors.ending_date_field).should('not.have.css', 'border-color', emailSendingPageUtils.error_color)
       cy.contains(emailSendingPageSelectors.agreement_error).should('be.visible').should('have.css', 'color', emailSendingPageUtils.error_color)
     })
+
+    it('tests validation of the email field', () => {
+      cy.contains(emailSendingPageUtils.email_field_error).should('not.exist')
+      cy.get(emailSendingPageSelectors.email_field).type('thisisnotvalid@')
+      cy.contains(emailSendingPageUtils.email_field_error).should('have.css', 'color', emailSendingPageUtils.error_color)
+      cy.get(emailSendingPageSelectors.email_field).clear().type('thisis@valid.com')
+      cy.contains(emailSendingPageUtils.email_field_error).should('not.exist')
+    })
+
+    it('tests the newsletter type field and options copy', () => {
+      cy.get(emailSendingPageSelectors.newsletter_type_field).click()
+      cy.get(emailSendingPageSelectors.newsletter_option_1).invoke('text').should('eq', emailSendingPageUtils.newsletter_option_1_copy)  
+      cy.get(emailSendingPageSelectors.newsletter_option_2).invoke('text').should('eq', emailSendingPageUtils.newsletter_option_2_copy)  
+      cy.get(emailSendingPageSelectors.newsletter_option_3).invoke('text').should('eq', emailSendingPageUtils.newsletter_option_3_copy)  
+      cy.get(emailSendingPageSelectors.newsletter_option_1).click().then(() => {
+        cy.get(emailSendingPageSelectors.newsletter_selected_option).invoke('text').should('eq', emailSendingPageUtils.newsletter_option_1_copy)
+      })
+      cy.get(emailSendingPageSelectors.newsletter_type_field).click()
+      cy.get(emailSendingPageSelectors.newsletter_option_2).click().then(() => {
+        cy.get(emailSendingPageSelectors.newsletter_selected_option).invoke('text').should('eq', emailSendingPageUtils.newsletter_option_2_copy)
+      })
+      cy.get(emailSendingPageSelectors.newsletter_type_field).click()
+      cy.get(emailSendingPageSelectors.newsletter_option_3).click().then(() => {
+        cy.get(emailSendingPageSelectors.newsletter_selected_option).invoke('text').should('eq', emailSendingPageUtils.newsletter_option_3_copy)
+      })
+    })
   })
