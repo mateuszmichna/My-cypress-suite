88b62fb54e6665b7e49af3977349bdf593fbdfc6
Mateusz <mat.michna@gmail.com>
2021-11-09 10:11:09 +0100

    delete the living example dependecies

--- cypress/fixtures/email_sending_page_selectors.js
+++
@@ -1,4 +0,0 @@
-export var someKindOfPageSelectors = {
-    //This is example of selectors collection connected with some kind of page
-    //use JSON formatting
-}
--- cypress/fixtures/test_page_utils.js
+++ cypress/fixtures/test_page_utils.js
@@ -1,3 +1,4 @@
 export var someKindOfPageUtils = {
     //Write here your consts ex. error copies or text contents of different elements. Use JSON format.
+    "example_element_copy_description": "copy_example"
 }
