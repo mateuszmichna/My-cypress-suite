e589b8fd714b86b17385dbcda308638a18420718
Mateusz <mat.michna@gmail.com>
2021-11-25 10:12:40 +0100

    clear repo

--- cypress/fixtures/test_page_selectors.js
+++
@@ -1,4 +0,0 @@
-export var someKindOfPageSelectors = {
-    //This is example of selectors collection connected with some kind of page. Use JSON formatting
-    "example_selector_descrtiption": "selector_example"
-}
--- cypress/fixtures/test_page_utils.js
+++
@@ -1,4 +0,0 @@
-export var someKindOfPageUtils = {
-    //Write here your consts ex. error copies or text contents of different elements. Use JSON format.
-    "example_element_copy_description": "copy_example"
-}
